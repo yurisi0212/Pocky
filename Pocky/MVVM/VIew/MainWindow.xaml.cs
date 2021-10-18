6a9f2195e6c4e2d1574227aa3eee079f1c3ab913
yurisi0212 <dev.yurisi@gmail.com>
2021-10-18 14:48:08 +0900

    プロジェクト ファイルを追加します。

---
+++ Pocky/MVVM/VIew/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MahApps.Metro.Controls;
+using Pocky.MVVM.ViewModel;
+
+namespace Pocky.MVVM.View {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow {
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel(this);
+        }
+    }
+}
---
+++ Pocky/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Pocky.MVVM.View;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Pocky.MVVM.ViewModel {
+    public class MainWindowViewModel : DependencyObject {
+
+        private MainWindow _parentWindow;
+
+        public static readonly DependencyProperty YoutubeURLTextProperty =
+            DependencyProperty.Register("TitleText", typeof(string), typeof(MainWindowViewModel), new UIPropertyMetadata(""));
+
+        public string YoutubeURLText {
+            get => (string)GetValue(YoutubeURLTextProperty);
+            set => SetValue(YoutubeURLTextProperty, value);
+        }
+
+        private ICommand OpenFolderCommand { get; }
+        private ICommand ExitCommand { get; }
+        private ICommand VersionInfoCommand { get; }
+
+        public MainWindowViewModel(MainWindow window) {
+            _parentWindow = window;
+
+            OpenFolderCommand = new RelayCommand(() => OpenFolder());
+            ExitCommand = new RelayCommand(() => Application.Current.Shutdown());
+            VersionInfoCommand = new RelayCommand(() => VersionInfo());
+;       }
+
+        private void OpenFolder() {
+
+        }
+
+        private void VersionInfo() {
+
+        }
+    }
+}
