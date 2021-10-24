b5ee747a0325b00b29371b0fbf82c2cd00aa23d7
yurisi0212 <Dev.yurisi@gmail.com>
2021-10-25 03:44:29 +0900

    ダウンロードボタンに関数の割当

--- Pocky/MVVM/ViewModel/MainWindowViewModel.cs
+++ Pocky/MVVM/ViewModel/MainWindowViewModel.cs
@@ -3,7 +3,6 @@ using MahApps.Metro.Controls.Dialogs;
 using Pocky.MVVM.Model;
 using Pocky.MVVM.View;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -25,7 +24,7 @@ namespace Pocky.MVVM.ViewModel {
         public ICommand OpenFolderCommand { get; }
         public ICommand ExitCommand { get; }
         public ICommand VersionInfoCommand { get; }
-        public ICommand DownLoadButtonCommand { get; }
+        public ICommand DownloadButtonCommand { get; }
 
         public MainWindowViewModel(MainWindow window) {
             _parentWindow = window;
@@ -34,19 +33,24 @@ namespace Pocky.MVVM.ViewModel {
             OpenFolderCommand = new RelayCommand(() => Process.Start("explorer.exe", _directory.Path));
             ExitCommand = new RelayCommand(() => Application.Current.Shutdown());
             VersionInfoCommand = new RelayCommand(() => VersionInfoAsync());
+            DownloadButtonCommand = new RelayCommand(() => Download());
         }
 
-        private async Task VersionInfoAsync() {
+        private async void VersionInfoAsync() {
             await _parentWindow.ShowMessageAsync(
-                "Pocky", 
-                "Pocky version 1.0.0\n\n" +
-                "Copylight(C)2021 yurisi All rights reserved.\n" +
-                "本ソフトウェアはJellyParfaitの一部機能を削除したものです。\n\n" +
-                "本ソフトウェアはオープンソースソフトウェアです。\n" +
-                "GPL-3.0 Licenseに基づき誰でも複製や改変ができます。\n\n" +
-                "Github\n" +
-                "https://github.com/yurisi0212/Pocky"
+                    "Pocky", 
+                    "Pocky version 1.0.0\n\n" +
+                    "Copylight(C)2021 yurisi All rights reserved.\n" +
+                    "本ソフトウェアはJellyParfaitの一部機能を削除したものです。\n\n" +
+                    "本ソフトウェアはオープンソースソフトウェアです。\n" +
+                    "GPL-3.0 Licenseに基づき誰でも複製や改変ができます。\n\n" +
+                    "Github\n" +
+                    "https://github.com/yurisi0212/Pocky"
                 );
+        }
+
+        private void Download() {
+
         }
     }
 }   
